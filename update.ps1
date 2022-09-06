4f1ac26267cabd893b4be295545affc2fab08dd5
Artem Moldovanenko <kryeker3@gmail.com>
2022-09-06 10:27:44 +0300

    fixes

--- update.ps1
+++ update.ps1
@@ -3,3 +3,4 @@ Set-PSDebug -Trace 1
 & dotnet paket update
 
 Copy-Item paket-files\acidanthera\OcBinaryData\Resources -Destination EFI\OC -Recurse -Force
+Copy-Item paket-files\RehabMan\OS-X-USB-Inject-All\XHCI-unsupported.kext -Destination EFI\OC\Kexts -Recurse -Force
